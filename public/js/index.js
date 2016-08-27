b27805f33361f6409cbea8993cccd16f1f7ac414
tanshuo <shuotan@ShuoTandeMac-mini.local>
2016-08-26 20:46:34 -0700

    view

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -9,29 +9,105 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var numOfPlayer = 0;
+var targetDOM = {};
+var players = [];
+var spys = [];
+var result = {};
+var broadcasterID = '';
+
+var parseResult = function parseResult(result) {
+  var keys = Object.keys(result);
+  players = new Array(numOfPlayer);
+  spys = [];
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var partsOfStr = key.split('_');
+    var type = partsOfStr[0];
+    var index = partsOfStr[1];
+    if (type == 'player') {
+      players[index] = result[key];
+    }
+  }
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var partsOfStr = key.split('_');
+    var type = partsOfStr[0];
+    var index = partsOfStr[1];
+    if (type == 'status') {
+      if (result[key] == 'on') {
+        spys.push(players[index]);
+      }
+    }
+  }
+};
 
 var getPlayerKeys = function getPlayerKeys(num) {
   var results = [];
   for (var i = 0; i < num; i++) {
-    results.push("player" + i);
+    results.push("player" + '_' + i);
   }
   return results;
 };
 
-var NumberOfPlayers = React.createClass({
-  displayName: "NumberOfPlayers",
+var getStatusKeys = function getStatusKeys(num) {
+  var results = [];
+  for (var i = 0; i < num; i++) {
+    results.push("status" + '_' + i);
+  }
+  return results;
+};
 
-  handleNumnberChange: function handleNumnberChange(e) {
-    numOfPlayer = e.target.value;
+var Broadcaster = React.createClass({
+  displayName: 'Broadcaster',
+
+  getInitialState: function getInitialState() {
+    return { broadcasterID: '' };
+  },
+  handleChange: function handleChange(e) {
+    broadcasterID = e.target.value;
+    this.setState({ broadcasterID: e.target.value });
   },
   render: function render() {
     return React.createElement(
-      "form",
-      { className: "commentForm" },
-      React.createElement("input", {
-        type: "number",
-        placeholder: numOfPlayer,
-        value: numOfPlayer,
+      'form',
+      { className: 'commentForm' },
+      React.createElement('input', {
+        type: 'text',
+        placeholder: 'empty',
+        value: this.state.broadcasterID,
+        onChange: this.handleChange
+      })
+    );
+  }
+});
+
+var NumberOfPlayers = React.createClass({
+  displayName: 'NumberOfPlayers',
+
+  getInitialState: function getInitialState() {
+    return { num: 0 };
+  },
+  handleNumnberChange: function handleNumnberChange(e) {
+    var target_value = e.target.value;
+
+    if (target_value < 0) {
+      target_value = 0;
+    } else if (target_value > 5) {
+      target_value = 5;
+    }
+
+    numOfPlayer = target_value;
+    this.setState({ num: target_value });
+    targetDOM.forceUpdate();
+  },
+  render: function render() {
+    return React.createElement(
+      'form',
+      { className: 'commentForm' },
+      React.createElement('input', {
+        type: 'number',
+        placeholder: 0,
+        value: this.state.num,
         onChange: this.handleNumnberChange
       })
     );
@@ -39,36 +115,50 @@ var NumberOfPlayers = React.createClass({
 });
 
 var Form = React.createClass({
-  displayName: "Form",
+  displayName: 'Form',
 
   getInitialState: function getInitialState() {
     var result = {};
-    var keys = getPlayerKeys();
+    var keys = getPlayerKeys(numOfPlayer);
+    var status = getStatusKeys(numOfPlayer);
     for (iter in keys) {
       result.set(iter, "");
     }
+    for (var iter in status) {
+      result.set(iter, false);
+    }
+    targetDOM = this;
     return result;
   },
 
-  handleChange: function handleChange(name, e) {
+  handleChange: function handleChange(type, name, e) {
     var change = {};
     change[name] = e.target.value;
     this.setState(change);
   },
   render: function render() {
+    result = this.state;
+    targetDOM = this;
     var indents = [];
     for (var i = 0; i < numOfPlayer; i++) {
-      indents.push(React.createElement("input", {
-        "data-tag": i,
-        type: "text",
-        placeholder: "Player ID...",
-        value: this.state.get("player" + i),
-        onChange: this.handleChange.bind(this, "player" + i)
+      indents.push(React.createElement('input', {
+        'data-tag': i,
+        type: 'text',
+        placeholder: 'Player ID...',
+        value: this.state["player" + '_' + i],
+        key: "player" + '_' + i,
+        onChange: this.handleChange.bind(this, 'player', 'player' + '_' + i)
+      }));
+      indents.push(React.createElement('input', {
+        type: 'checkbox',
+        key: "status" + '_' + i,
+        value: this.state["status" + '_' + i],
+        onChange: this.handleChange.bind(this, 'status', 'status' + '_' + i)
       }));
     }
     return React.createElement(
-      "form",
-      { className: "commentForm" },
+      'form',
+      { className: 'commentForm' },
       indents
     );
   }
@@ -87,18 +177,21 @@ var CreateButton = function (_React$Component) {
   }
 
   _createClass(CreateButton, [{
-    key: "handleClick",
-    value: function handleClick() {}
+    key: 'handleClick',
+    value: function handleClick() {
+      parseResult(result);
+      $.post("http://mofangvr.com:30001/api/spy/start", { players: players, spys: spys, broadcasterID: broadcasterID }, function (data) {
+        alert('page content: ' + data);
+      });
+    }
   }, {
-    key: "render",
+    key: 'render',
     value: function render() {
-      var text = this.state.liked ? 'liked' : 'haven\'t liked';
+
       return React.createElement(
-        "div",
-        { onClick: this.handleClick },
-        "You ",
-        text,
-        " this. Click to toggle."
+        'button',
+        { type: 'button', onClick: this.handleClick },
+        '开始游戏.'
       );
     }
   }]);
@@ -106,8 +199,30 @@ var CreateButton = function (_React$Component) {
   return CreateButton;
 }(React.Component);
 
-ReactDOM.render(React.createElement(NumberOfPlayers, { name: "NumberOfPlayers" }), document.getElementById('numofplayer'));
+var Compact = function (_React$Component2) {
+  _inherits(Compact, _React$Component2);
 
-ReactDOM.render(React.createElement(Form, { name: "CreateButton" }), document.getElementById('createbutton'));
+  function Compact() {
+    _classCallCheck(this, Compact);
 
-ReactDOM.render(React.createElement(Form, { name: "Form" }), document.getElementById('form'));
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Compact).apply(this, arguments));
+  }
+
+  _createClass(Compact, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(Broadcaster, null),
+        React.createElement(NumberOfPlayers, null),
+        React.createElement(Form, null),
+        React.createElement(CreateButton, null)
+      );
+    }
+  }]);
+
+  return Compact;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Compact, { name: 'Compact' }), document.getElementById('compact'));
--- public/src/index.js
+++ public/src/index.js
@@ -1,58 +1,155 @@
+
 var numOfPlayer = 0;
+var targetDOM = {};
+var players = [];
+var spys = [];
+var result = {};
+var broadcasterID = '';
+
+var parseResult = function(result){
+  var keys = Object.keys(result)
+  players = new Array(numOfPlayer);
+  spys = [];
+  for(var i = 0; i < keys.length; i++){
+    var key = keys[i];
+    var partsOfStr = key.split('_');
+    var type = partsOfStr[0];
+    var index = partsOfStr[1];
+    if(type == 'player'){
+      players[index] = result[key];
+    }
+  }
+   for(var i = 0; i < keys.length; i++){
+    var key = keys[i];
+    var partsOfStr = key.split('_');
+    var type = partsOfStr[0];
+    var index = partsOfStr[1];
+    if(type == 'status'){
+      if(result[key] == 'on'){
+        spys.push(players[index]);
+      }
+    }
+  }
+}
 
 var getPlayerKeys = function(num){
   var results = [];
   for(var i = 0; i < num; i++){
-    results.push("player" + i);
+    results.push("player" + '_' + i);
   }
   return results;
 };
 
-var NumberOfPlayers = React.createClass({
-    handleNumnberChange : function(e){
-      numOfPlayer = e.target.value;
+var getStatusKeys = function(num){
+  var results = [];
+  for(var i = 0; i < num; i++){
+    results.push("status" + '_' + i);
+  }
+  return results;
+};
+
+var Broadcaster = React.createClass({
+  getInitialState: function() {
+    return {broadcasterID : ''};
+  },
+    handleChange : function(e){
+      broadcasterID = e.target.value;
+      this.setState({broadcasterID : e.target.value});
     },
-     render: function(){
+    render: function(){
        return (
-      <form className="commentForm">
-        <input
-          type="number"
-          placeholder={numOfPlayer}
-          value={numOfPlayer}
-          onChange={this.handleNumnberChange}
-        />
-      </form>
-    );
-     }
+        <form className="commentForm">
+          <input
+            type="text"
+            placeholder='empty'
+            value={this.state.broadcasterID}
+            onChange={this.handleChange}
+          />
+        </form>
+      );
+    }
+});
+
+var NumberOfPlayers = React.createClass({
+  getInitialState: function() {
+    return {num : 0};
+  },
+    handleNumnberChange : function(e){
+      var target_value = e.target.value;
+      
+      if(target_value < 0){
+        target_value = 0;
+       
+      }
+      else if(target_value > 5){
+        target_value = 5;
+       
+      }
+      
+      numOfPlayer = target_value;
+      this.setState({num : target_value});
+      targetDOM.forceUpdate();
+    },
+    render: function(){
+       return (
+        <form className="commentForm">
+          <input
+            type="number"
+            placeholder={0}
+            value={this.state.num}
+            onChange={this.handleNumnberChange}
+          />
+        </form>
+      );
+    }
 });
 
 
 var Form = React.createClass({
   getInitialState: function() {
     var result = {};
-    var keys = getPlayerKeys();
+    var keys = getPlayerKeys(numOfPlayer);
+    var status = getStatusKeys(numOfPlayer);
     for(iter in keys){
       result.set(iter, "");
     }
+    for(var iter in status){
+      result.set(iter, false);
+    }
+    targetDOM = this;
     return result;
   },
  
-  handleChange: function(name, e) {
+  handleChange: function(type, name, e) {
     var change = {};
     change[name] = e.target.value;
     this.setState(change);
+    
   },
   render: function() {
+    result = this.state;
+    targetDOM = this;
     var indents = [];
     for (var i = 0; i < numOfPlayer; i++) {
      indents.push(
-       <input
+        <input
           data-tag={i}
           type="text"
           placeholder="Player ID..."
-          value={this.state.get("player" + i)}
-          onChange={this.handleChange.bind(this, "player" + i)}
-        />);
+          value={this.state[("player" + '_' + i)]}
+          key={"player" + '_' + i}
+          onChange={this.handleChange.bind(this, 'player', 'player' + '_' + i)}
+        />
+      );
+      indents.push(
+        <input 
+          type="checkbox" 
+          key={"status" + '_' + i}
+          value={this.state[("status" + '_' + i)]}
+          onChange={this.handleChange.bind(this, 'status', 'status' + '_' + i)}
+        />
+      );
+      
     }
     return (
       <form className="commentForm">
@@ -68,19 +165,50 @@ class CreateButton extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
-    
+    parseResult(result);
+    $.post(
+        "http://mofangvr.com:30001/api/spy/start",
+        {players : players, spys : spys, broadcasterID : broadcasterID},
+        function(data) {
+           alert('page content: ' + data);
+        }
+    );
   }
   render() {
-    const text = this.state.liked ? 'liked' : 'haven\'t liked';
+    
     return (
-      <div onClick={this.handleClick}>
-        You {text} this. Click to toggle.
-      </div>
+      <button type="button" onClick={this.handleClick}>
+        开始游戏.
+      </button>
     );
   }
 }
 
+class Compact extends React.Component {
+  render() {
+    return (
+      <div>
+        <Broadcaster
+         
+        />
+        <NumberOfPlayers
+         
+        />
+        <Form
+         
+        />
+        <CreateButton
+         
+        />
+      </div>
+    );
+  }
+  
+}
+
 ReactDOM.render(
-  <Form name="Form" />,
-  document.getElementById('form')
+  <Compact name="Compact" />,
+  document.getElementById('compact')
 );
+
+
